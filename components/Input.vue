0d38646f270b08aedd6cd692674ff3f21b0aeacc
Hugo Cox <h.n.c.cox@gmail.com>
2022-09-09 15:32:22 +0200

    initial commit

---
+++ components/Input.vue
@@ -0,0 +1,37 @@
+<template>
+	<label :for="id">{{label}}</label>
+	<input :type="type" :class="classes" :style="style">
+</template>
+
+<script lang="ts">
+import './button.css';
+import { reactive, computed } from 'vue';
+
+export type InputTypes = 'button' | 'checkbox' | 'color' | 'date' | 'datetime-local' | 'email' | 'file' | 'hidden' | 'image' | 'month' | 'number' | 'password' | 'radio' | 'range' | 'reset' | 'search' | 'submit' | 'tel' | 'text' | 'time' | 'url' | 'week';
+
+type Props = {
+	type: {
+		type: String,
+		required: true,
+	}
+}
+
+export default {
+	name: 'input',
+
+	emits: ['click'],
+
+	setup(props: Props, { emit }) {
+		props = reactive(props);
+		return {
+			classes: computed(() => ({
+			})),
+			style: computed(() => ({
+			})),
+			onClick() {
+				emit('click');
+			}
+		}
+	}
+}
+</script>
